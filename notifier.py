e41c1b2502004813352ebd9935328dd259891519
amir-ai1386 <amirbidel75@gmail.com>
2026-07-26 21:50:20 +0330

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,1 +1,19 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
 
+
+def send_message(message):
+    """
+    ارسال پیام به تلگرام
+    """
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    response = requests.post(url, data=data)
+
+    return response.json()
