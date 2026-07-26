c1ad19eac0e67d0f79b2512923c91063939cca27
amir-ai1386 <amirbidel75@gmail.com>
2026-07-26 21:46:33 +0330

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,1 +1,19 @@
+def analyze_market(klines):
+    """
+    تحلیل اولیه بازار
+    """
 
+    if not klines:
+        return None
+
+    last_close = float(klines[-1][4])
+    previous_close = float(klines[-2][4])
+
+    if last_close > previous_close:
+        return "LONG"
+
+    elif last_close < previous_close:
+        return "SHORT"
+
+    else:
+        return "NO TRADE"
