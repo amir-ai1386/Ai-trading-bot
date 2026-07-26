b0e6968647aaf229ed294f4d696a61aae8969aa7
amir-ai1386 <amirbidel75@gmail.com>
2026-07-26 21:27:48 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+import os
 
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+# Binance
+BASE_URL = "https://api.binance.com"
+
+# Symbols
+SYMBOLS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "XRPUSDT",
+    "SOLUSDT"
+]
+
+# Timeframes
+TIMEFRAMES = [
+    "15m",
+    "1h",
+    "4h"
+]
