88f7116d0fe67fdf94cd64c3bfd0dc6c6be84a8f
amir-ai1386 <amirbidel75@gmail.com>
2026-07-26 21:39:56 +0330

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,23 @@
+import requests
+from config import BASE_URL
 
+
+def get_klines(symbol, interval, limit=200):
+    """
+    دریافت کندل‌های بازار از Binance
+    """
+
+    url = f"{BASE_URL}/api/v3/klines"
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+
+    return None
