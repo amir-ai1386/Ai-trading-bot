6adb4650bc5b421bc3cb70dddfb3847f2cf50018
amir-ai1386 <amirbidel75@gmail.com>
2026-07-26 21:55:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from config import SYMBOLS, TIMEFRAMES
+from data import get_klines
+from strategy import analyze_market
+from notifier import send_message
 
+
+def run():
+
+    for symbol in SYMBOLS:
+
+        for timeframe in TIMEFRAMES:
+
+            klines = get_klines(symbol, timeframe)
+
+            signal = analyze_market(klines)
+
+            message = (
+                f"📊 {symbol}\n"
+                f"⏰ Timeframe: {timeframe}\n"
+                f"📈 Signal: {signal}"
+            )
+
+            print(message)
+
+            # ارسال به تلگرام
+            # send_message(message)
+
+
+if __name__ == "__main__":
+    run()
