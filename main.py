0402dab495fe0aa916d2d18c63c2b6af68fcb0f4
amir-ai1386 <amirbidel75@gmail.com>
2026-07-29 19:01:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,44 @@ from config import SYMBOLS, TIMEFRAMES
 from data import get_klines
 from strategy import analyze_market
 from notifier import send_message
+from database import create_tables, save_trade
 
 
 def run():
+
+    create_tables()
 
     for symbol in SYMBOLS:
 
         for timeframe in TIMEFRAMES:
 
-            klines = get_klines(symbol, timeframe)
+            try:
 
-            signal = analyze_market(klines)
+                klines = get_klines(symbol, timeframe)
 
-            message = (
-                f"📊 {symbol}\n"
-                f"⏰ Timeframe: {timeframe}\n"
-                f"📈 Signal: {signal}"
-            )
+                signal = analyze_market(klines)
 
-            print(message)
+                if signal == "NO TRADE":
+                    continue
 
-            # ارسال به تلگرام
-            # send_message(message)
+                message = (
+                    f"📊 {symbol}\n"
+                    f"⏰ Timeframe: {timeframe}\n"
+                    f"📈 Signal: {signal}"
+                )
+
+                save_trade(
+                    symbol=symbol,
+                    timeframe=timeframe,
+                    signal=signal
+                )
+
+                print(message)
+
+                send_message(message)
+
+            except Exception as e:
+                print(f"{symbol} {timeframe} -> {e}")
 
 
 if __name__ == "__main__":
